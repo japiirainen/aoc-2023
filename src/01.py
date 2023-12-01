3ac0a49d71115f3ec0a56e1c276ce18061b3dfeb
japiirainen <joona.piirainen@gmail.com>
2023-12-01 08:06:47 +0200

    feat: solve day 01

--- src/01.py
+++ src/01.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
-input = open(0).read()
+import re
 
-print(input)
+input = open(0).read().strip().splitlines()
+
+t = 0
+for line in input:
+    digits = re.findall(r"\d", line)
+    t += int(digits[0] + digits[-1])
+
+print(f"Part 1: {t}")
+
+n = "one two three four five six seven eight nine".split()
+r = "(?=(" + "|".join(n) + "|\\d))"
+
+t = 0
+for line in input:
+    digits = [str(n.index(x) + 1) if x in n else x for x in re.findall(r, line)]
+    t += int(digits[0] + digits[-1])
+
+print(f"Part 2: {t}")
