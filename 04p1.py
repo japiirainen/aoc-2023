a48541c214d63c39bffdd834a6ef999c2ca269cd
japiirainen <joona.piirainen@gmail.com>
2023-12-04 18:41:34 +0200

    chore(04): small simplification

--- 04p1.py
+++ 04p1.py
@@ -2,7 +2,7 @@
 
 t = 0
 
-for line in open(0).read().strip().splitlines():
+for line in open(0):
     a, b = [set(map(int, x.strip().split())) for x in line.split(": ")[1].split(" | ")]
     n = len(a & b)
     if n > 0:
