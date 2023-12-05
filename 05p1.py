beb09fbc42817c82efac824394e49de92704b78a
japiirainen <joona.piirainen@gmail.com>
2023-12-05 19:13:57 +0200

    refactor(05): more cleanup

--- 05p1.py
+++ 05p1.py
@@ -4,16 +4,18 @@ seeds, *rest = open(0).read().split("\n\n")
 
 seeds = [int(x) for x in seeds.split(": ")[1].split()]
 
+
+def f(ranges, x):
+    for end, start, n in ranges:
+        if x in range(start, start + n):
+            return x + end - start
+    else:
+        return x
+
+
 for g in rest:
     xs = g.strip().split("\n")[1:]
     ranges = [[int(y) for y in x.split()] for x in xs]
-    new = []
-    for seed in seeds:
-        for dest, src, n in ranges:
-            if seed in range(src, src + n):
-                new.append(seed - src + dest)
-        else:
-            new.append(seed)
-    seeds = new
+    seeds = [f(ranges, x) for x in seeds]
 
 print(min(seeds))
--- 05p2.py
+++ 05p2.py
@@ -6,25 +6,28 @@ i = [int(x) for x in i.split(": ")[1].split()]
 
 seeds = [(i[j], i[j] + i[j + 1]) for j in range(0, len(i), 2)]
 
-overlap = lambda start, end, src, n: (max(start, src), min(end, src + n))
+overlap = lambda ss, se, s, n: (max(ss, s), min(se, s + n))
+
+
+def f(ranges, seeds):
+    while seeds:
+        ss, se = seeds.pop()
+        for e, s, n in ranges:
+            os, oe = overlap(ss, se, s, n)
+            if os < oe:
+                yield (os - s + e, oe - s + e)
+                if se > oe:
+                    seeds.append((oe, se))
+                if ss < os:
+                    seeds.append((ss, os))
+                break
+        else:
+            yield (ss, se)
+
 
 for g in rest:
     xs = g.strip().split("\n")[1:]
     ranges = [[int(y) for y in x.split()] for x in xs]
-    new = []
-    while seeds:
-        s, e = seeds.pop()
-        for dest, src, n in ranges:
-            os, oe = overlap(s, e, src, n)
-            if os < oe:
-                new.append((os - src + dest, oe - src + dest))
-                if os > s:
-                    seeds.append((s, os))
-                if e > oe:
-                    seeds.append((oe, e))
-                break
-        else:
-            new.append((s, e))
-    seeds = new
+    seeds = list(f(ranges, seeds))
 
 print(min(seeds)[0])
