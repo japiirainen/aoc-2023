0b72a1f486d6a6f7c39e08e6911ae05f85722503
japiirainen <joona.piirainen@gmail.com>
2023-12-05 19:16:47 +0200

    refactor(05p2.py): remove redundant overlap lambda

--- 05p2.py
+++ 05p2.py
@@ -6,14 +6,12 @@ i = [int(x) for x in i.split(": ")[1].split()]
 
 seeds = [(i[j], i[j] + i[j + 1]) for j in range(0, len(i), 2)]
 
-overlap = lambda ss, se, s, n: (max(ss, s), min(se, s + n))
-
 
 def f(ranges, seeds):
     while seeds:
         ss, se = seeds.pop()
         for e, s, n in ranges:
-            os, oe = overlap(ss, se, s, n)
+            os, oe = max(ss, s), min(se, s + n)
             if os < oe:
                 yield (os - s + e, oe - s + e)
                 if se > oe:
