44a75b0e669101903eb94a2ab5e13db7be1bee45
japiirainen <joona.piirainen@gmail.com>
2023-12-17 08:04:55 +0200

    feat: solve day 17

---
+++ src/17p1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import heapq
+
+grid = open(0).read().splitlines()
+R = len(grid)
+C = len(grid[0])
+
+sr, sc = 0, 0
+seen = {}
+q = [(0, sr, sc, 0, 0)]  # heat_loss, r, c, dir, straight_moves
+
+while q:
+    hl, r, c, dir_, sm = heapq.heappop(q)
+
+    if (r, c) == (R - 1, C - 1):
+        print(hl)
+        break
+
+    if (r, c, dir_, sm) in seen:
+        continue
+
+    seen[(r, c, dir_, sm)] = hl
+
+    for i, (dr, dc) in enumerate([(-1, 0), (0, 1), (1, 0), (0, -1)]):
+        rr, cc = r + dr, c + dc
+        new_dir = i
+        new_sm = 1 if dir_ != new_dir else sm + 1
+        can_move = ((new_dir + 2) % 4 != dir_) and new_sm <= 3
+        if 0 <= rr < R and 0 <= cc < C and can_move:
+            heapq.heappush(q, (hl + int(grid[rr][cc]), rr, cc, new_dir, new_sm))
---
+++ src/17p2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import heapq
+
+grid = open(0).read().splitlines()
+R = len(grid)
+C = len(grid[0])
+
+sr, sc = 0, 0
+seen = {}
+q = [(0, sr, sc, 0, -1)]  # heat_loss, r, c, dir, straight_moves
+
+while q:
+    hl, r, c, dir_, sm = heapq.heappop(q)
+
+    if (r, c) == (R - 1, C - 1):
+        print(hl)
+        break
+
+    if (r, c, dir_, sm) in seen:
+        continue
+
+    seen[(r, c, dir_, sm)] = hl
+
+    for i, (dr, dc) in enumerate([(-1, 0), (0, 1), (1, 0), (0, -1)]):
+        rr, cc = r + dr, c + dc
+        new_dir = i
+        new_sm = 1 if dir_ != new_dir else sm + 1
+        can_move = (
+            ((new_dir + 2) % 4 != dir_)  # can't turn 180 degrees
+            and new_sm <= 10
+            and (new_dir == dir_ or sm >= 4 or sm == -1)
+        )
+        if 0 <= rr < R and 0 <= cc < C and can_move:
+            heapq.heappush(q, (hl + int(grid[rr][cc]), rr, cc, new_dir, new_sm))
