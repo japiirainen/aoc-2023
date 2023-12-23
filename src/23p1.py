16c7dfb153e35fc13693d9796a21984abe10740a
japiirainen <joona.piirainen@gmail.com>
2023-12-23 15:02:41 +0200

    refactor(23): more readable solutions

--- src/23p1.py
+++ src/23p1.py
@@ -1,32 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 
 import sys
 
 grid = open(0).read().splitlines()
 R, C = len(grid), len(grid[0])
 
-sys.setrecursionlimit(100000)
+dirs = {
+    ".": ((0, 1), (0, -1), (1, 0), (-1, 0)),
+    "<": ((0, -1),),
+    ">": ((0, 1),),
+    "^": ((-1, 0),),
+    "v": ((1, 0),),
+}
+
+sys.setrecursionlimit(10000)
 
 
 def dfs(r, c, end, seen):
     if (r, c) == end:
         return len(seen)
-    ch = grid[r][c]
     best = 0
-    for nr, nc in [
-        (r + dr, c + dc)
-        for dr, dc in (
-            [(0, -1)]
-            if ch == "<"
-            else [(0, 1)]
-            if ch == ">"
-            else [(-1, 0)]
-            if ch == "^"
-            else [(1, 0)]
-            if ch == "v"
-            else [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        )
-    ]:
+    for nr, nc in [(r + dr, c + dc) for dr, dc in dirs[grid[r][c]]]:
         if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] != "#" and (nr, nc) not in seen:
             best = max(best, dfs(nr, nc, end, seen | {(nr, nc)}))
     return best
--- src/23p2.py
+++ src/23p2.py
@@ -1,41 +1,52 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 
 grid = open(0).read().splitlines()
-R, C, edges = len(grid), len(grid[0]), {}
+R, C = len(grid), len(grid[0])
+start, end = (0, grid[0].index(".")), (R - 1, grid[R - 1].index("."))
 
+points = [start, end]
+
+# perform [edge contraction](https://en.wikipedia.org/wiki/Edge_contraction) to prune the search space.
 for r, row in enumerate(grid):
     for c, ch in enumerate(row):
         if ch == "#":
             continue
-        for nr, nc in ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
-            if not (0 <= nr < R and 0 <= nc < C and grid[nr][nc] != "#"):
-                continue
-            edges.setdefault((r, c), set()).add((nr, nc, 1))
-            edges.setdefault((nr, nc), set()).add((r, c, 1))
+        ns = 0
+        for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+            if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] != "#":
+                ns += 1
+        if ns >= 3:
+            points.append((r, c))
 
-# remove nodes with degree 2 by mergin edges
-while True:
-    for n, e in edges.items():
-        if len(e) == 2:
-            a, b = e
-            edges[a[:2]] -= {n + (a[2],)}
-            edges[b[:2]] -= {n + (b[2],)}
-            edges[a[:2]] |= {(b[0], b[1], a[2] + b[2])}
-            edges[b[:2]] |= {(a[0], a[1], a[2] + b[2])}
-            del edges[n]
-            break
-    else:
-        break
+graph = {point: {} for point in points}
 
-
-def dfs(r, c, d, seen):
-    if (r, c) == (R - 1, C - 2):
-        return d
-    best = 0
-    for nr, nc, l in edges[(r, c)]:
-        if (nr, nc) not in seen:
-            best = max(best, dfs(nr, nc, d + l, seen | {(nr, nc)}))
-    return best
+for sr, sc in points:
+    stack: list[tuple[int, int, int]] = [(0, sr, sc)]
+    seen = {(sr, sc)}
+    while stack:
+        n, r, c = stack.pop()
+        if n != 0 and (r, c) in points:
+            graph[(sr, sc)][(r, c)] = n
+            continue
+        for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+            if (
+                0 <= nr < R
+                and 0 <= nc < C
+                and grid[nr][nc] != "#"
+                and (nr, nc) not in seen
+            ):
+                seen.add((nr, nc))
+                stack.append((n + 1, nr, nc))
 
 
-print(dfs(0, 1, 0, set()))
+def dfs(cur, seen=set()):
+    if cur == end:
+        return 0
+    m = -float("inf")
+    for n in graph[cur]:
+        if n not in seen:
+            m = max(m, dfs(n, seen | {cur}) + graph[cur][n])
+    return m
+
+
+print(dfs(start))
